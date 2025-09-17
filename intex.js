829524eaccc5dc1c514c9ea0010edab19d821876
Sundar1207 <sundarvirat17@gmail.com>
2025-09-17 19:14:13 +0530

    Math Object

--- intex.js
+++ intex.js
@@ -1,61 +1,15 @@
-// CharAt
-let str="JavaScript"
-console.log(str.charAt(0));
+console.log(Math.PI);        // 3.141592653589793
+console.log(Math.sqrt(16));  // 4
+console.log(Math.pow(2, 3)); // 8
+console.log(Math.abs(-7));   // 7
+console.log(Math.round(4.6)); // 5
+console.log(Math.ceil(4.2));  // 5
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(1,5,3)); // 1
+console.log(Math.max(1,5,3)); // 5
 
-// charcodeAt
-console.log(str.charCodeAt(3))
+// Random number between 0 and 1
+console.log(Math.random());
 
-// Case Converstion 
-
-// Upper case
-console.log(str.toUpperCase())
-
-// Lover case 
-console.log(str.toLowerCase())
-
-// Searching  
-var str1="Hello World "
-console.log(str1.indexOf("e"))
-console.log(str1.lastIndexOf("o"))
-console.log(str1.includes("Hello"))
-console.log(str1.startsWith("Hell"))
-console.log(str1.endsWith("rld "))
-
-// Extracting Substrings
-
-console.log(str1.slice(-6))
-console.log(str1.substring(0,5))
-
-// Replacing
-
-var str2="I love Program"
-console.log(str2.replace("Program","Python"))
-console.log(str2.replaceAll("o", "O"))
-
-// Trimming Spaces
-let str3= "   Hello   ";
-
-console.log(str3.trim());      // "Hello"
-console.log(str3.trimStart()); // "Hello   "
-console.log(str3.trimEnd());   // "   Hello"
-
-// Splitting & Joining
-
-let Furit= "apple,banana,orange";
-
-
-let f= console.log(Furit.split(","))
-
-let Fruits = ["Apple", "Mango", "Orange"];
-console.log(Fruits.join("-"))
-
-// Repeating
-
-console.log(str3.repeat(3))
-
-// Template Literals (backticks `)
-
-console.log(`Hello ${str2}!`)
-console.log(str.concat(" ",str2))
-console.log(str3.padStart(10,"#"))
-console.log(str3.padEnd(10,"*"))
+// Random number between 1 and 10
+console.log(Math.floor(Math.random() * 10) + 1);
