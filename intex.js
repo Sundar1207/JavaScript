5f844604f358050611c582ed9d71a826a771a1e9
Sundar1207 <sundarvirat17@gmail.com>
2025-09-17 19:05:45 +0530

    String all function

--- intex.js
+++ intex.js
@@ -1,5 +1,61 @@
-let arr = [3, 1, 2];
+// CharAt
+let str="JavaScript"
+console.log(str.charAt(0));
 
-arr.sort();        // [1, 2, 3] (string sort by default)
-arr.reverse();     // [3, 2, 1]
-arr.sort((a,b) => b - a); // [3, 2, 1] numeric sort
+// charcodeAt
+console.log(str.charCodeAt(3))
+
+// Case Converstion 
+
+// Upper case
+console.log(str.toUpperCase())
+
+// Lover case 
+console.log(str.toLowerCase())
+
+// Searching  
+var str1="Hello World "
+console.log(str1.indexOf("e"))
+console.log(str1.lastIndexOf("o"))
+console.log(str1.includes("Hello"))
+console.log(str1.startsWith("Hell"))
+console.log(str1.endsWith("rld "))
+
+// Extracting Substrings
+
+console.log(str1.slice(-6))
+console.log(str1.substring(0,5))
+
+// Replacing
+
+var str2="I love Program"
+console.log(str2.replace("Program","Python"))
+console.log(str2.replaceAll("o", "O"))
+
+// Trimming Spaces
+let str3= "   Hello   ";
+
+console.log(str3.trim());      // "Hello"
+console.log(str3.trimStart()); // "Hello   "
+console.log(str3.trimEnd());   // "   Hello"
+
+// Splitting & Joining
+
+let Furit= "apple,banana,orange";
+
+
+let f= console.log(Furit.split(","))
+
+let Fruits = ["Apple", "Mango", "Orange"];
+console.log(Fruits.join("-"))
+
+// Repeating
+
+console.log(str3.repeat(3))
+
+// Template Literals (backticks `)
+
+console.log(`Hello ${str2}!`)
+console.log(str.concat(" ",str2))
+console.log(str3.padStart(10,"#"))
+console.log(str3.padEnd(10,"*"))
