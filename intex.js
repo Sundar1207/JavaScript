a64b3d613d3a49261a697dccae784e6b446622d1
Sundar1207 <sundarvirat17@gmail.com>
2025-09-17 11:30:30 +0530

    Spread & Rest

--- intex.js
+++ intex.js
@@ -1,13 +1,32 @@
-// var - function scope
+//  Spread Operatore and Rest Operatore
+// spread operatoe - Used to expand/unpack elements of an array, string, or object into individual elements.
+// Rest Operatore - Used to collect/pack multiple arguments into a single array or object.
 
-function names() {
-    if (true) {   // lowercase true, condition inside ()
-        var x = 10;   // declared with var inside block
-        console.log("Inside if: ", x);
-    }
+let name= "Sundar";
+let name1="Moorthy"
+let fullname=[...name," ",...name1]
+console.log(fullname);
 
-    // var is function-scoped → still accessible here
-    console.log("Outside if: ", x);
+let arr=[1,2,3,4,5]
+let arr1=[...arr,...name]
+
+console.log(arr1)
+
+let Emp = {
+    Name :fullname,
+    Reg:192324228,
+    Dep:"AI-DS"
 }
 
-names();
+let id={...Emp, Phone : 9783276324};
+console.log(id)
+// Rest Operatore 
+
+function SumOf(arr2){
+
+    let sum=arr2.reduce((a,b)=>a+b,0)
+    console.log(sum)
+}
+
+let arr2 =[1,2,3,4,5,6]
+SumOf(arr2)
