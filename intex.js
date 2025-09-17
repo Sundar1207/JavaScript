966e106ff0ca612c4306235404565d6fc8c907b5
Sundar1207 <sundarvirat17@gmail.com>
2025-09-17 12:49:55 +0530

    Looping / Iterating - Array

--- intex.js
+++ intex.js
@@ -1,16 +1,8 @@
-//  Arrayb methods 
-let arr=[100,200,300,400]
+let arr = [1, 2, 3];
 
-arr.pop()    // Pop()- Remove the last element in array , No arrgument passing 
-arr.push(500,600)  //push() - insert the value at last index in array , can feat argument 
-console.log(arr)
-
-arr.unshift(0); //unshift can inset the value at index 0 or first elements 
-console.log(arr) 
-arr.shift();   //shift only caling function no arguments , remove the value  at index 0 or first elements
-console.log(arr)
-
-arr.splice(2,1)  // splice(x,y,z)- x: start position , y - no of deleting elements , insert elemnts 
-console.log(arr);
-arr.splice(2, 0, [300,400])
-console.log(arr);
+arr.forEach(x => console.log(x));  // 1 2 3
+arr.map(x => x * 2);  // [2, 4, 6]
+arr.filter(x => x > 1); // [2, 3]
+arr.reduce((a, b) => a + b, 0); // 6
+arr.some(x => x > 2); // true
+arr.every(x => x > 0); // true
